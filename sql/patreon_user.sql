bfdfec106927375127df5ddb4c4c927928a9a7c1
Dave Humphrey <uespnet@gmail.com>
2020-01-08 14:19:25 -0500

    add token fields to database

--- sql/patreon_user.sql
+++ sql/patreon_user.sql
@@ -1,5 +1,8 @@
 CREATE TABLE /*$wgDBprefix*/patreon_user (
   user_patreonid DECIMAL(25,0) unsigned NOT NULL PRIMARY KEY,
   user_id int(10) unsigned NOT NULL,
+  access_token TINYTEXT NOT NULL,
+  refresh_token TINYTEXT NOT NULL,
+  token_expires TIMESTAMP NOT NULL,
   KEY(user_id)
 ) /*$wgDBTableOptions*/;
